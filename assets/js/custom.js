db6d48a58edd02111f84bd14189e6402568460da
Parth <mparth141@gmail.com>
2024-07-19 13:49:35 +0530

    add apis

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,3 +39,32 @@ function decryptData(encryptedData, key, iv) {
     
     return decryptedData;
 }
+
+function getCategory() {
+    $.get("https://gamersaim.com/Apis/games_spot_web/get_category.php", function(data, status){
+        // console.log("Data: " + data + "\nStatus: " + status);
+        if(status == 'success'){
+            // var key = "mvjfhcbgrjdknclgoidhcjsyrkalswuq";
+            // var iv = "bhdtfycloudowbvh";
+
+            // var decryptedData = decryptData(data, key, iv);
+            // console.log(decryptedData);
+
+            let resData = JSON.parse(data);
+            let cat = resData.category;
+            let cat_html = "";
+            cat.forEach(function(catData) {
+                var type = "category";
+                var cat_name = catData.category;
+                if(catData.category == "New Releases"){ type = "new_releases";cat_name=""; }
+                else if(catData.category == "Trending"){ type = "trending";cat_name=""; }
+
+                cat_html += `<li class="category-item" id="cat_${catData.id}" onclick="getGames('${type}','${cat_name}')">
+                                <img src="${catData.img}" alt=" " class="menu-icon">
+                                <span class="menu-name">${catData.category}</span>
+                            </li>`;
+            });
+            $("#cat_list").html(cat_html);
+        }
+    });
+}
